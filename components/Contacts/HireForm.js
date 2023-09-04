7387ccb4ce382be31a17c10baac2f512e250ec33
Veronicandemo <ndemoveronica@gmail.com>
2023-09-04 21:28:36 +0300

    Added form and email submission

--- components/Contacts/HireForm.js
+++ components/Contacts/HireForm.js
@@ -1,103 +1,118 @@
 import React, { useRef, useState } from 'react'
 import classes from './HireForm.module.css'
 import Par from './Par'
+import emailjs from '@emailjs/browser'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function HireForm() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
+
   const [inputData, setInputData] = useState({})
   const [messageIsSent, setMessageIsSent] = useState(false)
   const [nameIsValid, setNameIsValid] = useState(false)
   const [emailIsValid, setEmailIsValid] = useState(false)
   const [subjectIsValid, setSubjectIsValid] = useState(false)
   const [messageIsValid, setMessageIsValid] = useState(false)
-  const nameInputRef = useRef()
-  const emailInputRef = useRef()
-  const subjectInputRef = useRef()
-  const messageInputRef = useRef()
-  function formSubmitHandler(e) {
-    e.preventDefault()
-    if (nameInputRef.current.value == '') {
-      setNameIsValid(true)
-      return
-    } else if (emailInputRef.current.value == '') {
-      setEmailIsValid(true)
-      return
-    } else if (subjectInputRef.current.value === '') {
-      setSubjectIsValid(true)
-      return
-    } else if (messageInputRef.current.value === '') {
-      setMessageIsSent(true)
-      return
-    }
-    setInputData((prevState) => {
-      return {
-        ...prevState,
-        name: nameInputRef.current.value,
-        email: emailInputRef.current.value,
-        subject: subjectInputRef.current.value,
-        message: messageInputRef.current.value,
-      }
-    })
-    // console.log(inputData);
-    function sendEmail() {
-      Email.send({
-        Host: 'smtp.gmail.com',
-        Username: 'ndemoveronica@gmail.com',
-        Password: 'veetnyavvkscfobo',
-        To: `ndemoveronica@gmail.com`,
-        From: `ndemoveronica@gmail.com`,
-        Subject: `${inputData.name},${inputData.email}:${inputData.subject}`,
-        Body: `${inputData.message}`,
-      }).then(function (message) {
-        setMessageIsSent(true)
-        setInputData((prevState) => {
-          return {
-            ...prevState,
-            name: '',
-            email: '',
-            subject: '',
-            message: '',
-          }
-        })
-      })
-    }
-    setEmailIsValid(false)
-    setNameIsValid(false)
-    setMessageIsValid(false)
-    setSubjectIsValid(false)
-    sendEmail()
+
+  const form = useRef()
+  const sendEmail = (e) => {
+    e.preventDefault() // prevents the page from reloading when you hit “Send”
+
+    emailjs
+      .sendForm(
+        'service_3u9vrih',
+        'template_ygybki8',
+        form.current,
+        'CpuitAMc1whVN5PCK',
+      )
+      .then(
+        (result) => {
+          toast.success('Message Sent ☑️', {
+            position: 'top-right',
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'dark',
+          })
+        },
+        (error) => {
+          // show the user an error
+          console.log('Email not sent')
+          toast.error('Message not sent ❌', {
+            position: 'top-center',
+
+            theme: 'dark',
+          })
+        },
+      )
+    setName('')
+    setEmail('')
+    setMessage('')
+    setSubject('')
   }
+
   return (
-    <form className={classes.form}>
-      <input
-        value={inputData.name}
-        ref={nameInputRef}
-        type="text"
-        placeholder="Your name*"
-        style={{ borderColor: nameIsValid && '#f9004d' }}
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        limit={1}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
       />
-      <input
-        value={inputData.email}
-        ref={emailInputRef}
-        type="email"
-        placeholder="Your email*"
-        style={{ borderColor: emailIsValid && '#f9004d' }}
-      />
-      <input
-        value={inputData.subject}
-        ref={subjectInputRef}
-        type="text"
-        placeholder="Write a subject"
-        style={{ borderColor: subjectIsValid && '#f9004d' }}
-      />
-      <textarea
-        ref={messageInputRef}
-        value={inputData.message}
-        placeholder="Your Message"
-        style={{ borderColor: messageIsValid && '#f9004d' }}
-        rows={5}
-      />
-      <button onClick={formSubmitHandler}>Submit</button>
-      {messageIsSent && <Par>Your messegae has been sent succesfuly </Par>}
-    </form>
+      <form ref={form} onSubmit={sendEmail} className={classes.form}>
+        <input
+          value={name}
+          name="user_name"
+          onChange={(e) => setName(e.target.value)}
+          // ref={nameInputRef}
+          type="text"
+          placeholder="Your name*"
+          style={{ borderColor: nameIsValid && '#f9004d' }}
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          // ref={emailInputRef}
+          type="email"
+          name="user_email"
+          placeholder="Your email*"
+          style={{ borderColor: emailIsValid && '#f9004d' }}
+        />
+        <input
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+          name="subject"
+          // ref={subjectInputRef}
+          type="text"
+          placeholder="Write a subject"
+          style={{ borderColor: subjectIsValid && '#f9004d' }}
+        />
+        <textarea
+          // ref={messageInputRef}
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          placeholder="Your Message"
+          name="message"
+          style={{ borderColor: messageIsValid && '#f9004d' }}
+          rows={5}
+        />
+        <button onClick={sendEmail}>Submit</button>
+        {messageIsSent && <Par>Your messegae has been sent succesfuly </Par>}
+      </form>
+    </>
   )
 }
