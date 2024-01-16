dfe1d4b3ea63ce6990a8da4f91fffab97a3572c2
Veronicandemo <ndemoveronica@gmail.com>
2024-01-16 09:42:47 +0300

    Added the project to projects section

--- components/Contacts/Contacts.js
+++ components/Contacts/Contacts.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import classes from './Contacts.module.css'
-import HireForm from './HireForm'
-import Image from 'next/image'
+import React from "react";
+import classes from "./Contacts.module.css";
+import HireForm from "./HireForm";
+import Image from "next/image";
 
 export default function Contacts() {
   return (
     <div className={classes.contact} id="Contact">
       <div className={classes.form}>
         <h1>Hire Me.</h1>
-        <p>
+        <div>
           I am available for freelance work or any full time job.Connect with me
           via phone:<a href="tel:0708219438">&nbsp;0112490586</a> or just email
-          me through{' '}
+          me through{" "}
           <a href="mailto:ndemoveronica@gmail.com">
             &nbsp;ndemoveronica@gmail.com
           </a>
           <HireForm />
-        </p>
+        </div>
       </div>
       {/* <h1 color="#fff">Bug</h1> */}
       {/* <div>
         <Image alt="picture of veronica Ndemo" />
       </div> */}
     </div>
-  )
+  );
 }
