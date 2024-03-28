feb8f9e06e512589f19ffd2174038496a2f54b83
Veronicandemo <ndemoveronica@gmail.com>
2024-03-28 09:00:50 +0300

    Project Update

--- components/Projects/data.js
+++ components/Projects/data.js
@@ -20,27 +20,27 @@ export const projects = [
       "https://media.istockphoto.com/id/637711198/photo/hand-with-marker-writing-skill-concept.jpg?s=1024x1024&w=is&k=20&c=lMLbXlQAYI-fMEfNSA_qJgiM3KjrXE1H3Lmh2lY8Ivo=",
     projectUrl: "https://grito-website-two-point-zero.vercel.app/",
   },
-  // {
-  //   subHeading: "Live",
-  //   title: "Sulmuc",
-  //   imageUrl:
-  //     "https://media.istockphoto.com/id/1254718662/photo/cloud-computing-technology-and-online-data-storage-for-business-network-concept.jpg?s=1024x1024&w=is&k=20&c=K55lNv8sjFVnI6fa4a0vctQ9Aj9DNOAOsPKUjvs85Cc=",
-  //   projectUrl: "https://sulmuc.vercel.app/",
-  // },
-  // {
-  //   subHeading: "Live",
-  //   title: "Morbell Solutions",
-  //   imageUrl:
-  //     "https://media.istockphoto.com/id/1187179171/photo/futuristic-office.jpg?s=1024x1024&w=is&k=20&c=iLSIZ87E2Ofy2GoGY_Kyii0ql_MR_EmTpyYDZJkkOHM=",
-  //   projectUrl: "https://morbell.vercel.app/",
-  // },
-  // {
-  //   subHeading: "Development",
-  //   title: "Styler",
-  //   imageUrl:
-  //     "https://media.istockphoto.com/id/1311600080/photo/small-shipping-packages-on-a-notebook-with-the-inscription-online-shopping.jpg?s=1024x1024&w=is&k=20&c=VvROXDiVybH5cjLcumXAcyKlzZJ11hCSVr1_abHs42w=",
-  //   projectUrl: "https://react-typescript-e-commerce-template.vercel.app/",
-  // },
+  {
+    subHeading: "Live",
+    title: "Sulmuc",
+    imageUrl:
+      "https://media.istockphoto.com/id/1254718662/photo/cloud-computing-technology-and-online-data-storage-for-business-network-concept.jpg?s=1024x1024&w=is&k=20&c=K55lNv8sjFVnI6fa4a0vctQ9Aj9DNOAOsPKUjvs85Cc=",
+    projectUrl: "https://sulmuc.vercel.app/",
+  },
+  {
+    subHeading: "Live",
+    title: "Morbell Solutions",
+    imageUrl:
+      "https://media.istockphoto.com/id/1187179171/photo/futuristic-office.jpg?s=1024x1024&w=is&k=20&c=iLSIZ87E2Ofy2GoGY_Kyii0ql_MR_EmTpyYDZJkkOHM=",
+    projectUrl: "https://morbell.vercel.app/",
+  },
+  {
+    subHeading: "Development",
+    title: "Styler",
+    imageUrl:
+      "https://media.istockphoto.com/id/1311600080/photo/small-shipping-packages-on-a-notebook-with-the-inscription-online-shopping.jpg?s=1024x1024&w=is&k=20&c=VvROXDiVybH5cjLcumXAcyKlzZJ11hCSVr1_abHs42w=",
+    projectUrl: "https://react-typescript-e-commerce-template.vercel.app/",
+  },
   {
     subHeading: "Development",
     title: "Travel List",
